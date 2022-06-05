c5b8457c26b0a52b01fd51e212275bdf2ee9711d
soohyunee <105588175+soohyuneee@users.noreply.github.com>
2022-06-05 18:36:54 +0900

    Add files via upload

---
+++ AccountingIfApp.java
@@ -0,0 +1,39 @@
+
+public class AccountingIfApp {
+
+	public static void main(String[] args) {
+		 
+		double valueOfSupply = Double.parseDouble(args[0]);
+		double vatRate = 0.1;
+		double expenseRate = 0.3;
+		double vat = valueOfSupply * vatRate;
+		double total = valueOfSupply + vat;
+		double expense = valueOfSupply * expenseRate;
+		double income = valueOfSupply - expense;
+		
+		double dividend1;
+		double dividened2;
+		double dividened3;
+		
+		if(income > 10000.0) {
+			dividend1 = income * 0.5;
+			dividened2 = income * 0.3;
+			dividened3 = income * 0.2;
+		} else {
+			dividend1 = income * 1.0;
+			dividened2 = income * 0;
+			dividened3 = income * 0;
+		}
+		
+		System.out.println("Value of supply : "+valueOfSupply);
+		System.out.println("VAT : "+ vat);
+		System.out.println("Total : "+ total);
+		System.out.println("Expense : "+ expense);
+		System.out.println("Income : "+ income);	
+		System.out.println("Dividend 1 : "+ dividend1);
+		System.out.println("Dividend 2 : "+ dividened2);
+		System.out.println("Dividend 3 : "+ dividened3);
+
+	}
+
+}
