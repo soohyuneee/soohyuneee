39774b0bd7fb0ddbac34a1e93656c77462cec363
soohyunee <105588175+soohyuneee@users.noreply.github.com>
2022-06-11 20:52:08 +0900

    Add files via upload

--- MyOOP.java
+++ MyOOP.java
@@ -1,14 +1,12 @@
 public class MyOOP {
 	public static void main(String[] args) {
-	Print p1 = new Print();
-	p1.delimiter = "----";
+	Print p1 = new Print("----");
 	p1.A();
 	p1.A();
 	p1.B();
 	p1.B();
 	
-	Print p2 = new Print();
-	p2.delimiter = "****";
+	Print p2 = new Print("****");
 	p2.A();
 	p2.A();
 	p2.B();
---
+++ StaticApp.java
@@ -0,0 +1,35 @@
+class Foo{
+	public static String classVar = "I class var";
+	public String instanceVar = "I instance Var";
+	public static void classMethod() {
+		System.out.println(classVar); // OK
+//		System.out.println(instanceVar); // Error
+	}
+	public void instanceMethod() {
+		System.out.println(classVar); // OK
+		System.out.println(instanceVar); // OK
+	}
+}
+public class StaticApp {
+
+	public static void main(String[] args) {
+		System.out.println(Foo.classVar); // OK
+//		System.out.println(Foo.instanceVar); // Error
+		Foo.classMethod();
+//		Foo.instaceMethod();
+		
+		Foo f1 = new Foo();
+		Foo f2 = new Foo();
+		System.out.println(f1.classVar);
+		System.out.println(f1.instanceVar);
+		
+		f1.classVar = "changed by f1";
+		System.out.println(Foo.classVar);
+		System.out.println(f2.classVar);
+		
+		f1.instanceVar = "changed by f1";
+		System.out.println(f1.instanceVar);
+		System.out.println(f2.instanceVar);
+	}
+
+}
